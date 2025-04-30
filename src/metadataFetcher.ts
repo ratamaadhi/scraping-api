5ce340904a1007ef4402e2ba248412c291f55347
Ratama Adhi Nugraha <ratama.adhi@gmail.com>
2025-05-01 01:56:53 +0700

    fix: remove timeout

--- src/metadataFetcher.ts
+++ src/metadataFetcher.ts
@@ -44,8 +44,7 @@ async function getHTMLContent(url: string): Promise<string> {
       'User-Agent':
         'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36',
       'Accept-Encoding': 'gzip, deflate, br',
-    },
-    timeout: 10000,
+    }
   });
   return html;
 }
